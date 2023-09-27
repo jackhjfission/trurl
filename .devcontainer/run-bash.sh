212b6009b9a755ad4f58a6ab58b519151ad3084d
jackhjfission <jackhjfission@gmail.com>
2023-09-27 11:04:28 +0000

    Default to jovyan user. Mount gitconfig

--- .devcontainer/run-bash.sh
+++ .devcontainer/run-bash.sh
@@ -4,7 +4,7 @@ IMAGE_NAME="$(grep IMAGE_NAME .env | cut -d'=' -f2)"
 IMAGE_VERSION="$(grep IMAGE_VERSION .env | cut -d'=' -f2)"
 
 sudo docker run -it \
+    --mount type=bind,src=$HOME/.gitconfig,dst=/home/jovyan/.gitconfig \
     --mount type=bind,src=$(dirname $(readlink -f $0))/../,dst=/home/jovyan/work \
-    --user root \
     --env CONTAINER_NAME=${IMAGE_NAME}:${IMAGE_VERSION} \
     $IMAGE_NAME:$IMAGE_VERSION /bin/bash
