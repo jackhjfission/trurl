b0ee9749a22041e42ac906efa863838a0ead5871
jackhjfission <jackhjfission@gmail.com>
2023-09-29 13:42:18 +0000

    Install local package on start
    Gets the dependencies from the pyproject.toml
    and installs them in the conda env and pins them
    This is to ensure the local package continues
    to work when changes are made to the environment

--- .devcontainer/on-start-host.sh
+++ .devcontainer/on-start-host.sh
@@ -2,3 +2,9 @@
 
 echo copying git pre-commit hook script
 cp /home/jovyan/.pre-commit /home/jovyan/work/.git/hooks/pre-commit
+
+# install the local package
+pip install --no-deps --no-build-isolation -e /home/jovyan/work
+# pin dependencies for local package and install them
+chmod +x /home/jovyan/work/.devcontainer/pin-deps
+mamba install -y $(/home/jovyan/work/.devcontainer/pin-deps)
