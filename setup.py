8c8b1e7ca46b9975cba3a0bbbbc964fe07e469cb
jackhjfission <jackhjfission@gmail.com>
2023-09-29 12:09:34 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="trurl",
+    version="v0.0.0",
+    packages=find_packages(
+        include=["trurl*"],
+        exclude=["trurl.tests"],
+    ),
+    entry_points={
+        "console_scripts": [
+            "trurl = trurl.cli:main",
+        ]
+    },
+    python_requires=">=3.7",
+    install_requires=[
+        "click==8.*",
+    ],
+    description="Another python package for traceable and reproducible data science.",
+    author="Jack Coggins",
+    author_email="jackhjfission@gmail.com",
+    url="https://github.com/jackhjfission/trurl",
+    license_files="LICENSE",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
