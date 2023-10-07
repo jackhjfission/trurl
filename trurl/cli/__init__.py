463e9a72f0c0ea56b8ab58da44f8ee6571bb25bc
jackhjfission <jackhjfission@gmail.com>
2023-10-07 19:28:12 +0000

    Add a function to output version

--- trurl/cli/__init__.py
+++ trurl/cli/__init__.py
@@ -1,3 +1,5 @@
+__version__ = "v0.1.2"
+
 import click
 
 from .environment import conda_export
@@ -6,6 +8,12 @@ from .environment import conda_export
 @click.group()
 def main() -> None:
     pass
+
+
+@main.command()
+def version() -> None:
+    """Outputs the installed version of trurl."""
+    print(f"trurl=={__version__}")
 
 
 main.command(conda_export)
